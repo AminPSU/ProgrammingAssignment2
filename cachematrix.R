81f8439d25559d91b32cc1d311342bfb90dd447c
Amin <Gotvand@users.noreply.github.com>
2016-12-27 13:59:44 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions can be used to calculate inverse of a inversable matrix and store its result for
+## future calls, as a cashed value. Calculation just happen at first call and then, while the input matrix does
+## not change, the output will be retrieved from cache.
 
-## Write a short comment describing this function
+## makeCacheMatrix, takes matrix x as its input and the result will be a list of 4 function.
+## This function works exactly the same way as the example code, makeVector.
 
 makeCacheMatrix <- function(x = matrix()) {
 inv <- NULL
@@ -18,7 +20,9 @@ inv <- NULL
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function takes an makeCacheMatrix object as its input. At it first run, it will calculate the inverse
+## of that matrix, and both return it as result and store it in cache for future calls. From the second run, this function
+## will give us the message "getting cached data" and then the result will be printed.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
